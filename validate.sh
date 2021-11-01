dfb6580dd80401a8a6bd5e93d082d0e751968258
kahummer <pkahama@ona.io>
2021-11-01 15:51:22 +0300

    Exit with failure when atlest one test is invalid

--- validate.sh
+++ validate.sh
@@ -1,10 +1,17 @@
+cmd_status=0
 for dir in `ls moz`;
 do
     for subdir in `ls moz/$dir`;
     do
         if [ "${subdir##*.}" = "json" ];
             then
-                npx ajv -s "validation-schema/schema.json" -d "moz/$dir/$subdir" --strict=false;
+                npx ajv test -s validation-schema/schema.json -d "moz/$dir/$subdir" --valid --strict=false
+                test_status=$?
+                if [ "$test_status" -gt "0" ]; then
+                    cmd_status=$test_status
+                fi
         fi
     done
 done
+
+exit $cmd_status
